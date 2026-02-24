7c30549ec7b018a2036a5b088814ae3b998c16e8
ivankhomichkov <ivan.khomichkov@gmail.com>
2026-02-24 17:27:28 +0400

    #5 — LevelData SO + create 3 level assets

--- Assets/_Project/Scripts/Config/BlockConfigSO.cs
+++ Assets/_Project/Scripts/Config/BlockConfigSO.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 
 namespace MergeCubes.Config
 {
+    public enum BlockType
+    {
+        None = 0,
+        Fire = 1,
+        Water = 2,
+    }
+    
     [CreateAssetMenu(fileName = "New Block Config", menuName = "MergeCubes/Block Config", order = 0)]
     public class BlockConfigSO : ScriptableObject
     {
+        public BlockType BlockType;
         public Sprite Sprite;
         public AnimationClip IdleClip;
         public AnimationClip DestroyClip;
--- Assets/_Project/Scripts/Config/LevelDataSO.cs
+++ Assets/_Project/Scripts/Config/LevelDataSO.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace MergeCubes.Config
@@ -5,6 +6,16 @@ namespace MergeCubes.Config
     [CreateAssetMenu(fileName = "New Level Data", menuName = "MergeCubes/Level Data", order = 0)]
     public class LevelDataSO : ScriptableObject
     {
-        
+        public int Width;
+        public int Height;
+        public BlockType[] InitialBlocks;
+
+        private void OnValidate()
+        {
+            if (InitialBlocks.Length != Width * Height)
+            {
+                Debug.LogWarning("Not all initial blocks are assigned inside config", this);
+            }
+        }
     }
 }
