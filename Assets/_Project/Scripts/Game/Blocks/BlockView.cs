0986745f15320ec4bcbc590ae6f2d52f25e2695e
ivankhomichkov <ivan.khomichkov@gmail.com>
2026-02-25 21:42:31 +0400

    #16 - BlockView
    - enabled unitask support for dotween
    - added blockview as a prefab
    - added block view implementation
    - reorganize scene

--- Assets/_Project/Scripts/Game/Blocks/BlockView.cs
+++ Assets/_Project/Scripts/Game/Blocks/BlockView.cs
@@ -1,3 +1,4 @@
+using System;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using MergeCubes.Config;
@@ -6,19 +7,47 @@ using UnityEngine;
 
 namespace MergeCubes.Game.Blocks
 {
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
     public class BlockView : MonoBehaviour
     {
-        private BlockConfigSO _blockConfig;
+        private const string IDLE_ANIMATION_KEY = "Idle";
+        private const string DESTROY_ANIMATION_KEY = "Destroy";
+        private static readonly int DestroyAnimationHash = Animator.StringToHash(DESTROY_ANIMATION_KEY);
+
+
         private GridPosition _gridPosition;
 
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+        private BoxCollider2D _boxCollider2D;
 
-        public void SelfDestroy() =>
-            Destroy(gameObject);
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _boxCollider2D = GetComponent<BoxCollider2D>();
+        }
+
+        private void OnDestroy() =>
+            transform.DOKill();
 
         public void Initialize(BlockConfigSO blockConfig)
         {
-            _blockConfig = blockConfig;
+            _spriteRenderer.sprite = blockConfig.Sprite;
+
+            var animatorOverrides = new AnimatorOverrideController(_animator.runtimeAnimatorController)
+            {
+                [IDLE_ANIMATION_KEY] = blockConfig.IdleClip,
+                [DESTROY_ANIMATION_KEY] = blockConfig.DestroyClip,
+            };
+
+            _animator.runtimeAnimatorController = animatorOverrides;
         }
+
+        public void SelfDestroy() =>
+            Destroy(gameObject);
 
         public GridPosition GetGridPosition() =>
             _gridPosition;
@@ -26,15 +55,19 @@ namespace MergeCubes.Game.Blocks
         public void SetGridPosition(GridPosition gridPosition) =>
             _gridPosition = gridPosition;
 
-        public async UniTask MoveToAsync(Vector3 bWorldPos, float gameConfigBlockMoveDuration,
-            Ease gameConfigBlockMoveEase)
+        public async UniTask MoveToAsync(Vector3 position, float duration,
+            Ease ease) =>
+            await transform.DOMove(position, duration, true).SetEase(ease).ToUniTask();
+
+        public async UniTask SelfDestroyAnimatedAsync(float duration)
         {
-            throw new System.NotImplementedException();
+            SetInteractable(false);
+            _animator.SetTrigger(DestroyAnimationHash);
+            await UniTask.Delay(TimeSpan.FromSeconds(duration));
+            SelfDestroy();
         }
 
-        public async UniTask SelfDestroyAnimatedAsync(float gameConfigBlockDestroyDuration)
-        {
-            throw new System.NotImplementedException();
-        }
+        private void SetInteractable(bool isEnabled) =>
+            _boxCollider2D.enabled = isEnabled;
     }
 }
