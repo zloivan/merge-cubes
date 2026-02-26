abfb65b676eb10e800917cfd9c19a07ac4386201
ivankhomichkov <ivan.khomichkov@gmail.com>
2026-02-26 18:14:18 +0400

    ADD: updated GameLifetimeScope to to get complete injection graph

--- Assets/_Project/Scripts/Bootstrap/GameLifetimeScope.cs
+++ Assets/_Project/Scripts/Bootstrap/GameLifetimeScope.cs
@@ -1,28 +1,49 @@
+using Cinemachine;
 using MergeCubes.Config;
+using MergeCubes.Game.Balloons;
 using MergeCubes.Game.Board;
 using MergeCubes.Game.Level;
+using MergeCubes.GameInput;
 using MergeCubes.Utilities;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using MergeCubes.UI;
 
 namespace MergeCubes.Bootstrap
 {
     public class GameLifetimeScope : LifetimeScope
     {
-        [SerializeField] private GameConfigSO _gameConfigSO;
+        [SerializeField] private GameConfigSO _gameConfig;
+
+        [SerializeField] private InputService _inputService;
+        [SerializeField] private BoardView _boardView;
+        [SerializeField] private CameraFitter _cameraFitter;
+        [SerializeField] private BalloonsSpawner _balloonSpawner;
+        [SerializeField] private HUDController _hudController;
+
+        [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+        [SerializeField] private Camera _mainCamera;
 
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.RegisterComponent(_gameConfigSO).As<GameConfigSO>();
-            builder.Register<LevelRepository>(Lifetime.Singleton);
-            builder.RegisterComponentInHierarchy<CameraFitter>();
-            builder.RegisterComponentInHierarchy<Test.Test>();
-            builder.Register<NormalizationController>(Lifetime.Singleton);
+            builder.RegisterInstance(_gameConfig);
+            builder.RegisterInstance(_mainCamera);
+
             builder.Register<BoardModel>(Lifetime.Singleton);
+            builder.Register<SwipeValidator>(Lifetime.Singleton);
             builder.Register<GravityResolver>(Lifetime.Singleton);
             builder.Register<MatchFinder>(Lifetime.Singleton);
 
+            builder.Register<LevelRepository>(Lifetime.Singleton);
+            builder.Register<NormalizationController>(Lifetime.Singleton);
+            builder.Register<BoardController>(Lifetime.Singleton).AsImplementedInterfaces().AsSelf();
+
+            builder.RegisterComponent(_inputService);
+            builder.RegisterComponent(_boardView);
+            builder.RegisterComponent(_cameraFitter);
+            builder.RegisterComponent(_balloonSpawner);
+            builder.RegisterComponent(_hudController);
         }
     }
 }
