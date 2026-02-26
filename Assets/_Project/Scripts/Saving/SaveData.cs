058a722226c047310d9471c9f22d66b330f25000
ivankhomichkov <ivan.khomichkov@gmail.com>
2026-02-27 02:05:08 +0400

    #22 — SaveData model

--- Assets/_Project/Scripts/Saving/SaveData.cs
+++ Assets/_Project/Scripts/Saving/SaveData.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace MergeCubes.Saving
 {
+    [Serializable]
     public class SaveData
     {
-        
+        public int LevelIndex;
+        public int Width;
+        public int Height;
+        public int[] Blocks;
+        public int SaveVersion;
     }
 }
